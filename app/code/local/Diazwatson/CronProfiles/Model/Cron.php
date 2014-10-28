2ecff0b3b3b05cb48c05e71fc33119d95b63fca3
Raul Diaz <raul.diaz@interactiv4.com>
2014-10-28 02:34:39 +0100

    Added posibility disable indexing before run the profiles

--- app/code/local/Diazwatson/CronProfiles/Model/Cron.php
+++ app/code/local/Diazwatson/CronProfiles/Model/Cron.php
@@ -33,12 +33,43 @@ class Diazwatson_CronProfiles_Model_Cron extends Mage_Core_Model_Abstract
             $profiles = explode(",",Mage::getStoreConfig('i4cronprofiles/general/profile_ids'));
 
             if(!empty($profiles)){
+
+                if(Mage::getStoreConfig('i4cronprofiles/general/disable_index') )
+                {
+                    $this->disableAutomaticIndexing();
+                }
+
                 foreach ($profiles as $id) {
                     Mage::getModel('dataflow/profile')
                         ->load($id)
                         ->run();
                 }
+
+                $this->enableAutomaticIndexing();
+
+                /* @var $indexCollection Mage_Index_Model_Resource_Process_Collection */
+                $indexCollection = Mage::getModel('index/process')->getCollection();
+                foreach ($indexCollection as $index) {
+                    /* @var $index Mage_Index_Model_Process */
+                    $index->reindexAll();
+                }
             }
+        }
+    }
+
+    public function disableAutomaticIndexing()
+    {
+        $Collection = Mage::getSingleton('index/indexer')->getProcessesCollection();
+        foreach ($Collection as $process) {
+            $process->setMode(Mage_Index_Model_Process::MODE_MANUAL)->save();
+        }
+    }
+
+    public function enableAutomaticIndexing()
+    {
+        $Collection = Mage::getSingleton('index/indexer')->getProcessesCollection();
+        foreach ($Collection as $process) {
+            $process->setMode(Mage_Index_Model_Process::MODE_REAL_TIME)->save();
         }
     }
 }
